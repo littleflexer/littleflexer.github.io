4829bf673e0890e6c85dbc0e2f51ec63d507557c
littleflexer <43014213+littleflexer@users.noreply.github.com>
2018-09-21 13:33:28 -0600

    draw sheldon

--- drawsheldon/sketch.js
+++ drawsheldon/sketch.js
@@ -3,12 +3,16 @@
 // Sept 19th
 //
 // Extra for Experts:
-// I used the mouseWheel() to change the size of Sheldon
+// I used the mouseWheel() to change the size of Sheldon. After that, I threw in some spicy samba music to get a nice mood going while you DrawSheldon.
 let shell;
 let scaler;
 let music;
+let youngShell;
+let bazinga;
 
 function preload(){
+  bazinga = loadImage("assets/bazinga.png")
+  youngShell = loadImage("assets/young sheldon.png")
   shell = loadImage("assets/SHELDON-BIG-BANG-THEORY.png")
   music = loadSound("assets/Samba.wav")
 }
@@ -25,8 +29,15 @@ function draw() {
   if (keyIsPressed && keyCode === 32) {
    image(shell, mouseX, mouseY, shell.width * scaler , shell.height * scaler)
 
-
   }
+  else if (keyIsPressed && keyCode === 66) {
+    image(bazinga, mouseX, mouseY, bazinga.width * scaler , bazinga.height * scaler)
+  }
+
+  else if (keyIsPressed && keyCode === 86) {
+    image(youngShell, mouseX, mouseY, youngShell.width * scaler , youngShell.height * scaler)
+  }
+
   else if (keyCode === 88){
     background(255)
     image(shell, mouseX, mouseY, shell.width * scaler , shell.height * scaler)
