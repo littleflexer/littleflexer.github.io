a91728f6c2a1a7d89bdf204473d7d688a21f212e
littleflexer <43014213+littleflexer@users.noreply.github.com>
2018-10-02 13:26:06 -0600

    new stuff

--- movingRect/sketch.js
+++ movingRect/sketch.js
@@ -6,9 +6,17 @@
 // - describe what you did to take this project "above and beyond"
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
+  alert("WARNING: flashing colors")
 }
 
 function draw() {
+  for (let i=0; i< width; i+=5){
+    for(let j =0; j<height; j+=5){
+      fill(int(random(255)), int(random(255)), int(random(255)));
+      noStroke();
+      rect(i, j, 5, 5)
+    }
+  }
 
 }
