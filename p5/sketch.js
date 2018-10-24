d125c08562d244b4227a148ba97308f388f92985
littleflexer <43014213+littleflexer@users.noreply.github.com>
2018-10-24 13:36:10 -0600

    finished sheldon

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,32 +1,43 @@
 // Project Title
 // Your Name
 // Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
-let x;
-let dx;
+let numberOfRects;
 let rectWidth;
+let time = 0;
+let rects = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width/2;
-  dx = 5;
-  rectWidth = 200;
-
+  numberOfRects = width;
+  rectWidth = width / numberOfRects;
+  generateRectangles();
 }
+
 function draw() {
-  background(125, 0, 0);
+  background(255);
+  fill(0);
+  displayRects();
+}
 
-  //move rectangle
-  x += dx
-  //check if wall has been hideControls
-  if (x > width - rectWidth || x < 0) {
-    dx = dx * -1
+function displayRects() {
+  for (let i=0; i<rects.length; i++) {
+    rect(rects[i].x, rects[i].y, rects[i].width, rects[i].height);
   }
+}
 
-  //display rectqngle
-  fill(0,125,0);
-  rect(x,400,200,150);
+function generateRectangles() {
+  rects = [];
+  for (let i=0; i<numberOfRects; i++) {
+    let rectHeight = noise(time) * height;
+    let someRect = {
+      x: i * rectWidth,
+      y: height - rectHeight,
+      width: rectWidth,
+      height: rectHeight,
+    };
+
+    rects.push(someRect);
+    time += 0.01;
+  }
 }
