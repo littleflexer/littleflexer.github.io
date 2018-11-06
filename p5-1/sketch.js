e828a5d905e5eb55a6ed4a3bb832cfb5cf8f1748
littleflexer <43014213+littleflexer@users.noreply.github.com>
2018-11-06 13:56:08 -0600

    Attempts at fixing

--- p5-1/sketch.js
+++ p5-1/sketch.js
@@ -9,12 +9,19 @@ let rows = 4;
 let cols = 5;
 let grid;
 let cellSize;
+
 let titleBg;
+
+let rand1;
+let rand2;
+let rand3;
+let rand4;
+
 let questionValues = [[1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]];
 let stringOfQuestions1 = ["Who goes by the nickname 'Mr. WorldWide?'", "What is a canine?", "How many legs do dogs have?", "Big dogs or little dogs?"];
 let stringOfQuestions2 = ["What is the smallest dog?", "Who's smarter: A dog or a two year old?", "True or False: Dogs can help improve mood?", "Dogs or cats?"];
 let stringOfQuestions3 = ["SSBhbSBydW5uaW5nIG91dCBvZiBxdWVzdGlvbnMu", "Which of these are not a real famous dog: Loca the pug, gabe the dog, Airbud?", "https://www.base64encode.org/", "What is internet lingo for bark?"];
-let stringOfQuestions4 = ["Can newborn dogs born with open or closed ear canels?", "What does Corgi translate to?", "What Star Wars creature was modelled off of George Lucas's dog?", "Dogs?"];
+let stringOfQuestions4 = ["Are newborn dogs born with open or closed ear canels?", "What does Corgi translate to?", "What Star Wars creature was modelled off of George Lucas's dog?", "Dogs?"];
 
 function preload(){
   titleBg = loadImage("assets/Dogparty title thing.jpg");
@@ -69,21 +76,21 @@ function mouseClicked(){
   //find out which square is pressed and display a question
   let x = floor(mouseX / cellSize);
   let y = floor(mouseY / cellSize);
-  console.log( questionValues[y][x] ); //atom dosn't know 'console' is a thing. It still works
+  let rand1 = random(stringOfQuestions1);
+  let rand2 = random(stringOfQuestions2);
+  let rand3 = random(stringOfQuestions3);
+  let rand4 = random(stringOfQuestions4);
+  //console.log(questionValues[y][x] ); //atom dosn't know 'console' is a thing. It still works
   if (questionValues === 1){
-    let rand1 = random(stringOfQuestions1);
     console.log(rand1);
   }
   else if (questionValues === 2){
-    let rand2 = random(stringOfQuestions2);
     console.log(rand2);
   }
   else if (questionValues === 3){
-    let rand3 = random(stringOfQuestions3);
     console.log(rand3);
   }
   else if (questionValues === 4){
-    let rand4 = random(stringOfQuestions4);
-    console.log(rand4);
+    console.log(rand4)
   }
 }
